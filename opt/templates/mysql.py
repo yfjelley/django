6d241f77fde53a4a47e54863b8891d82a94adeb5
yfjelley <549636719@qq.com>
2014-09-13 09:32:50 +0800

    del unusefull file

--- opt/templates/mysql.py
+++ opt/templates/mysql.py
@@ -1,17 +1,22 @@
 #coding:utf-8
 #!/usr/bin/python
 import os
+import datetime
 import time
+from time import localtime
 import MySQLdb
 import xlrd
 import sys
 import json
-from datetime import date 
+from datetime import date
+import glob
 reload(sys)
 sys.setdefaultencoding("utf-8")
-#name={'媒体':'media','对应的推广地址':'site','对应地区':'addres','消费':'consume','点击':'click',\
+#name={'日期':'date'，'媒体':'media','对应的推广地址':'site','对应地区':'addres','消费':'consume','点击':'click',\
 #        '总有效量':'valide','总预约量':'appointment','总就诊量':'visit','当天无就诊量':'unvisit'}
 def excel2mysql(fi):
+    date_test=fi.split("_")[1].split(".")[0]
+    print date_test
     db = MySQLdb.connect("localhost","root","123","mysite",charset='utf8')
     cursor = db.cursor()
     l=[]
@@ -28,32 +33,61 @@ def excel2mysql(fi):
         for col in range(0,ncols):
             if row==0:
                 t.append(sh.cell(0,col).value)
+                print sh.cell(0,col).value
             else:
-                t1.append(sh.cell(row,col).value)
+                if col ==0:
+                   __s_date = datetime.date(1899, 12, 31).toordinal()-1
+                   d=datetime.date.fromordinal(__s_date+int(sh.cell(row,col).value))
+                   value=d.strftime('%Y-%m-%d')
+                   if value != date_test:
+                       print "error:value equal date_test!"
+                       break
+                   t1.append(value)
+                else:
+                   t1.append(sh.cell(row,col).value)
         if row>0:
             z.append(zip(t,t1))
     s=z
     try:
         for i in range(0,len(s)):
-            r=cursor.execute('select * from opt_optimization where date(date) = curdate()')
+            r=cursor.execute('select * from opt_optimization where date(date) = "%s"'%value)
             print r
             count=cursor.execute('select * from opt_optimization limit 0,%d'%r)
             t=cursor.fetchall()
             print "*****:%s"%count
             flag1=0
-            if len(s[i])==9:
-                sql="insert into opt_optimization values (null,\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',null);\
-                        "%(s[i][0][1],s[i][1][1],s[i][2][1],s[i][3][1],s[i][4][1],s[i][5][1],s[i][6][1],s[i][7][1],s[i][8][1])
+            if len(s[i])==10:
+                print "*****",s[i][4][1],s[i][5][1],s[i][6][1],s[i][7][1],s[i][8][1]
+                if s[i][4][1] ==0 or s[i][4][1]=="":
+                    valide_cost=click_cost=appointment_cost=visit_cost=0
+                else:
+                    if isinstance(s[i][5][1],float) and int(s[i][5][1])>0:
+                        click_cost=round(s[i][4][1]/s[i][5][1],2)
+                    else:
+                        print "xxxx",type(s[i][5][1])
+                        click_cost=s[i][4][1]
+                    if isinstance(s[i][6][1],float) and int(s[i][6][1])>0:
+                        valide_cost=round(s[i][4][1]/s[i][6][1],2)
+                    else:
+                        valide_cost=s[i][4][1]
+                    if isinstance(s[i][7][1],float) and int(s[i][7][1])>0:
+                        appointment_cost=round(s[i][4][1]/s[i][7][1],2)
+                    else:
+                        appointment_cost=s[i][4][1]
+                    if isinstance(s[i][8][1],float) and int(s[i][8][1])>0:
+                        visit_cost=round(s[i][4][1]/s[i][8][1],2)
+                    else:
+                        visit_cost=s[i][4][1]
+                sql="insert into opt_optimization values (null,\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\');\
+                            "%(s[i][0][1],s[i][1][1],s[i][2][1],s[i][3][1],s[i][4][1],s[i][5][1],click_cost,s[i][6][1],s[i][7][1],s[i][8][1],valide_cost,\
+                            appointment_cost,visit_cost,s[i][9][1])
+
             print sql
             if count >0:
                 for j in  t:
-                    if j[1] == s[i][0][1] and j[2] == s[i][1][1]  and j[3] == s[i][2][1] :
-                                print "xxxxxx"
-                                flag1=1
-                    """if j[1] == s[i][0][1] and j[2] == s[i][1][1]  and j[3] == s[i][2][1] and j[4] == s[i][3][1]  and j[5] == s[i][4][1] \
-                            and j[6] == s[i][5][1] and j[7] == s[i][6][1] and j[8] == s[i][7][1] and  j[9] == s[i][8][1] :
-                                print "xxxxxx"
-                                flag1=1"""
+                    if j[2] == s[i][1][1] and j[3] == s[i][2][1]  and j[4] == s[i][3][1] :
+                        print "j[1]:%s s[i][0][1]:%s"%(j[1],s[i][0][1])
+                        flag1=1
 
             if  count ==0 or flag1==0:
                 try:
@@ -71,11 +105,12 @@ def excel2mysql(fi):
 
     db.close()
 def updateMysql():
-    f='youhua_%s.xlsx'%date.today()
-    print f
-    if os.path.isfile('/home/btc/data/changjiang/%s'%f) :
-        print "yyyy"
-        excel2mysql('/home/btc/data/changjiang/%s'%f)
+    f=[]
+    for filename in glob.glob("/home/btc/data/changjiang/youhua_*"):
+        f.append(filename)
+        if os.path.isfile('%s'%filename) :
+           print "yyyy"
+           excel2mysql('%s'%filename)
 if __name__ == "__main__":
     while(1):
         updateMysql()
