f0700a8c1e1121b3e745b77e247b1165853adabf
yfjelley <549636719@qq.com>
2014-09-15 09:50:56 +0800

    fix day report style

--- opt/views.py
+++ opt/views.py
@@ -59,23 +59,48 @@ def whichWeek(d):
 def dayReport(request):
     '''明细(日)视图'''
     params = request.POST.copy()
-    condition = {}
-    for k,v in params.iteritems():
-        v = v.strip()
-        if v != 'all':
-            condition[k] = v
-    a = Optimization.objects.filter(**condition)
-    logger.info("a:%s"%a)    
+    date,condition = paramsHandle(params)
+    p = dayTableData(date,condition)
+    if request.method == "GET":
+        return render_to_response('day.html',{'tableInfo':p},context_instance = RequestContext(request))
+    elif request.method == "POST":
+        return render_to_response('table.html',{'tableInfo':p},context_instance = RequestContext(request))
+def dayAccountReport(request):
+    '''明细(日)视图'''
+    params = request.POST.copy()
+    date,condition = paramsHandle(params)
+    p = dayAccountTableData(date,condition)
+    if request.method == "GET":
+        return render_to_response('dayAccount.html',{'tableInfo':p},context_instance = RequestContext(request))
+    elif request.method == "POST":
+        return render_to_response('tableAccount.html',{'tableInfo':p},context_instance = RequestContext(request))
+def dayAccountTableData(date,condition):
     p=[]
+    if date:
+        condition['date'] = date
+    a = Optimization.objects.filter(**condition).values('date','department','media','site').annotate(total_cusume=Sum('cusume'),\
+            total_click=Sum('click'),total_valide=Sum('valide'),total_appointment=Sum('appointment'),total_visit=Sum('visit'),\
+                        total_unvisit=Sum('unvisit')).order_by('date','department','media','site')
+    for i in a:
+        l = [i['date'],i['department'],i['media'],i['site'],i['total_cusume'],i['total_click'],round(i['total_cusume']/i['total_click'],2),\
+                i['total_valide'],i['total_appointment'],i['total_visit'],round(i['total_cusume']/i['total_valide'],2),round(i['total_cusume']/i['total_appointment'],2),\
+                round(i['total_cusume']/i['total_visit'],2),i['total_unvisit']]
+        p.append(l)
+    return p
+
+def dayTableData(date,condition):
+    p=[]
+    if date:
+        condition['date'] = date
+    a = Optimization.objects.filter(**condition)
+    logger.info("a:%s"%a)
     for i in a:
         l = [i.date,i.department,i.media,i.site,i.addres,i.cusume,i.click,round(i.cusume/i.click,2),\
                 i.valide,i.appointment,i.visit,round(i.cusume/i.valide,2),round(i.cusume/i.appointment,2),\
                 round(i.cusume/i.visit,2),i.unvisit]
         p.append(l)
-    if request.method == "GET":
-        return render_to_response('day.html',{'tableInfo':p},context_instance = RequestContext(request))
-    elif request.method == "POST":
-        return render_to_response('table.html',{'tableInfo':p},context_instance = RequestContext(request))
+    return p
+
 def weekList():
     '''生成周日历(201401...)'''
     nWeek = []
@@ -98,15 +123,16 @@ def paramsHandle(params):
             condition[k] = v
     return date,condition
 def tableData(date,week,condition,group_by,nWeek):
+    '''获取表格数据'''
     p = []
     if date:
         condition["date__range"] = (week[int(date[0][-2:])-1][0],week[int(date[0][-2:])-1][6]) 
-        p = weekSearch(condition,p,date[0],group_by)
+        p = weekSearch(condition,date[0],group_by)
     else:
         for w in nWeek:
             d = week[int(w[-2:])-1]
             condition["date__range"] = (d[0],d[len(d)-1])
-            p = weekSearch(condition,p,w,group_by)
+            p = weekSearch(condition,w,group_by)
     return p
 def weekReport(request):
     '''明细(周)视图'''
@@ -122,7 +148,7 @@ def weekReport(request):
     elif request.method == "POST":
         return render_to_response("selectWeek.html",{'nWeek':nWeek,'tableInfo':p},context_instance = RequestContext(request))
 
-def weekReportAccount(request):
+def weekAccountReport(request):
     '''站点汇总(周)'''
     a = []
     p = []
@@ -136,13 +162,30 @@ def weekReportAccount(request):
         return render_to_response('weekAccount.html',{'nWeek':nWeek,'tableInfo':p},context_instance = RequestContext(request))
     elif request.method == "POST":
         return render_to_response("selectWeekAccount.html",{'nWeek':nWeek,'tableInfo':p},context_instance = RequestContext(request))
-def weekSearch(c,o,w,group_by):
+def weekSearch(c,w,group_by):
     '''以周为单位汇总数据'''
+    o = []
     a = Optimization.objects.filter(**c).values(*group_by).annotate(total_cusume=Sum('cusume'),\
             total_click=Sum('click'),total_valide=Sum('valide'),total_appointment=Sum('appointment'),total_visit=Sum('visit'),\
             total_unvisit=Sum('unvisit')).order_by(*group_by)
     for i in a:
         l = [w]
+        for g in group_by:
+            l.append(i["%s"%g])
+        s = [i['total_cusume'],i['total_click'],round(i['total_cusume']/i['total_click'],2),\
+                i['total_valide'],i['total_appointment'],i['total_visit'],round(i['total_cusume']/i['total_valide'],2),\
+                round(i['total_cusume']/i['total_appointment'],2),round(i['total_cusume']/i['total_visit'],2),i['total_unvisit']]
+        l += s
+        o.append(l)
+    return o
+def daySearch(c,group_by):
+    '''以周为单位汇总数据'''
+    o = []
+    a = Optimization.objects.filter(**c).values(*group_by).annotate(total_cusume=Sum('cusume'),\
+            total_click=Sum('click'),total_valide=Sum('valide'),total_appointment=Sum('appointment'),total_visit=Sum('visit'),\
+            total_unvisit=Sum('unvisit')).order_by(*group_by)
+    for i in a:
+        l = []
         for g in group_by:
             l.append(i["%s"%g])
         s = [i['total_cusume'],i['total_click'],round(i['total_cusume']/i['total_click'],2),\
